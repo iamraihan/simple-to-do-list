020234ed6189cfb5003c49965fa9e73bb930255b
iamraihan <trueraihan@gmail.com>
2022-02-16 19:04:31 +0600

    extra btn added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ document.getElementById('add-task-btn').addEventListener('click', function () {
     if (taskInput == '') {
         alert('Please Input a task...')
     } else {
+
+        // add Task
         let tableContainer = document.getElementById('table-container')
         let tableContent = document.createElement('tr')
         tableContent.innerHTML = `                    
@@ -15,16 +17,24 @@ document.getElementById('add-task-btn').addEventListener('click', function () {
         <button class="btn btn-primary done-btn">Done</button></td>
         `
         tableContainer.appendChild(tableContent)
+        // empty input 
         document.getElementById('task-input').value = ''
+        // remove all event 
+        document.getElementById('empty-btn').addEventListener('click', function () {
+            tableContainer.innerText = ''
+        })
 
+
+        // button 
         let deleteButton = document.getElementsByClassName('delete-btn')
         let doneButton = document.getElementsByClassName('done-btn')
-
+        // remove button
         for (const deleteBtn of deleteButton) {
             deleteBtn.addEventListener('click', function (e) {
                 e.target.parentNode.parentNode.style.display = 'none'
             })
         }
+        //Done button
         for (const doneBtn of doneButton) {
             doneBtn.addEventListener('click', function (e) {
                 e.target.parentNode.parentNode.style.textDecoration = 'line-through'
