8a2d7ff1c7e328eb91df696ed54609852fe1a905
iamraihan <trueraihan@gmail.com>
2022-02-16 16:43:53 +0600

    event ad

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('add-task-btn').addEventListener('click', function () {
+    let taskInput = document.getElementById('task-input').value
+    if (taskInput == '') {
+        alert('Please Input a task...')
+    } else {
+        document.getElementById('table-container')
+    }
+})
